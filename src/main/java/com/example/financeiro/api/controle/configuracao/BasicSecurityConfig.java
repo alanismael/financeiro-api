8a94e64a55b98915aee72d5b728c746447396e8c
Alan Ismael <allanismael@gmail.com>
2020-05-25 20:22:00 -0300

    Correção de autenticação

--- src/main/java/com/example/financeiro/api/controle/configuracao/BasicSecurityConfig.java
+++ src/main/java/com/example/financeiro/api/controle/configuracao/BasicSecurityConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import io.swagger.models.HttpMethod;
 
 @Profile("basic-security")
 @EnableWebSecurity
@@ -31,10 +34,11 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/**").permitAll()
 			.anyRequest().authenticated().and()
 			.httpBasic().and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.csrf().disable();
 	}
-
+	
 }
