e1661c910a4501c7a5c63c9e508f3177cc1bb351
Alan Ismael <allanismael@gmail.com>
2020-05-26 22:22:57 -0300

    Correção da autenticação básica

--- src/main/java/com/example/financeiro/api/controle/configuracao/BasicSecurityConfig.java
+++ src/main/java/com/example/financeiro/api/controle/configuracao/BasicSecurityConfig.java
@@ -2,10 +2,11 @@ package com.example.financeiro.api.controle.configuracao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,8 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import io.swagger.models.HttpMethod;
 
+@Configuration
 @Profile("basic-security")
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -34,11 +35,11 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/**").permitAll()
+			.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+			.antMatchers(HttpMethod.GET,"/**").permitAll()
 			.anyRequest().authenticated().and()
 			.httpBasic().and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.csrf().disable();
+			.csrf().disable();			
 	}
-	
 }
